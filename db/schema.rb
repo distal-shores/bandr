2f5cdf818d4ca10148f03458786233b14051ec1a
distal-shores <samcoll@gmail.com>
2016-02-21 21:03:04 +0000

    schema shit

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,19 @@
-ActiveRecord::Schema.define(version: 20160221050831) do
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160221084228) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "band_users", force: :cascade do |t|
@@ -18,8 +32,10 @@ ActiveRecord::Schema.define(version: 20160221050831) do
   end
 
   create_table "connections", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "from_user_id"
+    t.integer  "to_user_id"
   end
 
   create_table "demo_links", force: :cascade do |t|
@@ -52,9 +68,15 @@ ActiveRecord::Schema.define(version: 20160221050831) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.boolean  "read"
+    t.integer  "connection_id"
+    t.integer  "user_id"
   end
+
+  add_index "messages", ["connection_id"], name: "index_messages_on_connection_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.boolean  "admin"
