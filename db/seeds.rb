70e2f7520de94e6fd609c072369475243feeaae6
pr0biem <coleson.luterbach@gmail.com>
2016-02-22 22:18:11 +0000

    added faker gem, deleted stupid fake connections

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 20.times do User.create([{ 
  email: Faker::Internet.email, 
- firstname: Faker::Name.first_name, 
- lastname: Faker::Name.last_name, 
+ first_name: Faker::Name.first_name, 
+ last_name: Faker::Name.last_name, 
  password_digest: Faker::Internet.password(6), 
  admin: false }]) 
 end
