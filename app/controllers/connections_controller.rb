8e7698736a252ad5f4dfcf872cfca7df0dea1c5e
applebits <joeross.ca@gmail.com>
2016-02-22 00:49:04 +0000

    working on messages

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -8,23 +8,42 @@ class ConnectionsController < ApplicationController
 
   def new
     @users = User.all conditions: ["id != ?", current_user.id]
+    %ul
+  - for user in @friends
+    %li
+      - if current_user.friend_with? user
+        = user.email
+        |
+        You are already friends!
+      - elsif current_user.invited? user
+        = user.email
+        |
+        Pending request ...
+      - elsif user.invited? current_user
+        = user.email
+        |
+        = link_to "Confirm friend?", friend_path(user), :method => "put"
+      - else
+        = user.email
+        |
+        = link_to "Add friend?", friends_path(:user_id => user), :method => "post"
   end
 
   def create
     invitee = User.find_by_id(params[:user_id])
     if current_user.invite invitee
-      redirect_to new_friend_path, notice: "Connection request sent!"
+      redirect_to new_friend_path, notice: "Connection request sent."
     else
-      redirect_to new_friend_path, notice: "Sorry! Could not send connection request!"
+      redirect_to new_friend_path, notice: "Sorry, could not send connection request."
     end
   end
 
   def update
     inviter = User.find_by_id(params[:id])
     if current_user.approve inviter
-      redirect_to new_connection_path, notice: "Connection confirmed!"
+      redirect_to new_connection_path, notice: "Connection confirmed."
     else
-      redirect_to new_connection_path, notice: "Sorry! Could not connect!"
+      redirect_to new_connection_path, notice: "Sorry, could not connect."
     end
   end
 
@@ -39,9 +58,9 @@ class ConnectionsController < ApplicationController
   def destroy
     user = User.find_by_id(params[:id])
     if current_user.remove_connection user
-      redirect_to connecitons_path, notice: "Successfully removed connection!"
+      redirect_to connecitons_path, notice: "Successfully removed connection."
     else
-      redirect_to connections_path, notice: "Sorry, couldn't remove connection!"
+      redirect_to connections_path, notice: "Sorry, couldn't remove connection."
     end
   end
 
