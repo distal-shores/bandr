3cf1e7cc2ab94f17569e1d4ad801f7c8db857b4c
distal-shores <samcoll@gmail.com>
2016-02-22 01:34:53 +0000

    added sign up, login, log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
     if @user.save
       session[:user_id] = @user.id
-      redirect_to bands_path, notice: "Welcome aboard, #{@user.first_name}!"
+      redirect_to @user, notice: "Welcome aboard, #{@user.first_name}!"
+    else
+      render 'new'
     end
 
   end
@@ -34,15 +35,15 @@ class UsersController < ApplicationController
 
   private
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+      def set_user
+        @user = User.find(params[:id])
+      end
 
-    def user_params
-      params.require(:user).permit(:admin, :first_name, :last_name, :email, :password, :password_confirmation
-                                  # oauth params :provider, :uid, :name, :oauth_token, :oauth_expires_at
-                                  )
-    end
+      def user_params
+        params.require(:user).permit(:admin, :first_name, :last_name, :email, :password, :password_confirmation
+                                    # oauth params :provider, :uid, :name, :oauth_token, :oauth_expires_at
+                                    )
+      end
     
 end
 
