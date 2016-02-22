bc264505882cb854e16793a9c499e3027f705cbf
distal-shores <samcoll@gmail.com>
2016-02-22 19:42:28 +0000

    resetting to previous HEAD

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,18 @@
 class SessionsController < ApplicationController
 
+
   def create
-
-    user = User.find_by(email: params[:email])
-
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to bands_path, notice: "Welcome back, #{user.first_name}!"
-    else
-      flash.now[:alert] = "Log in failed..."
-      render :new
-    end
-
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to bands_path, notice: "Adios!"
+    redirect_to root_path
   end
 
 end
-
-
 
 
  
