dfb1bb26547dd1216ff6eef3be9ba05398b7845b
pr0biem <coleson.luterbach@gmail.com>
2016-02-24 01:32:32 +0000

    accept function working properly. No longer shows add friend if already friends or request sent

--- app/helpers/connections_helper.rb
+++ app/helpers/connections_helper.rb
@@ -1,2 +1,17 @@
 module ConnectionsHelper
+  
+
+  def show_non_friends(user)
+    @user = Connection.where("status != '' AND user_id = ? AND friend_id = ?", current_user.id, user.id)
+    if @user
+      return @user
+    end
+  end
+
+  def show_pending_friends(user)
+    @accept = Connection.where('friend_id = ? AND user_id = ? AND status = ?', current_user.id, user.id, 'pending')
+    if @accept
+      return @accept
+    end
+  end
 end
