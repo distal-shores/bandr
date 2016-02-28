e7b3c2e41255406328b5cc14b8da7f1b9eca3390
distal-shores <samcoll@gmail.com>
2016-02-28 01:40:37 +0000

    landing page layout changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
    if user && user.authenticate(params[:password])
      session[:user_id] = user.id
-     redirect_to users_path, notice: "Welcome back, #{user.first_name}!"
+     redirect_to root_path
    else
      flash.now[:alert] = "Log in failed..."
      render :new
